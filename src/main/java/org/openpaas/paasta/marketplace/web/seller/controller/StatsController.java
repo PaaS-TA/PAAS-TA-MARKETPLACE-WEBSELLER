beeadbfcab8e393f997588d5f5235b734a039342
sjchoi <sjchoi@bluedigm.com>
2019-08-30 17:58:50 +0900

    판매자 요금 조회

--- src/main/java/org/openpaas/paasta/marketplace/web/seller/controller/StatsController.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/seller/controller/StatsController.java
@@ -1,16 +1,19 @@
 package org.openpaas.paasta.marketplace.web.seller.controller;
 
-import com.sun.org.glassfish.external.statistics.Stats;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.openpaas.paasta.marketplace.api.domain.*;
 import org.openpaas.paasta.marketplace.web.seller.common.CommonService;
-import org.openpaas.paasta.marketplace.web.seller.service.SoftwareService;
 import org.openpaas.paasta.marketplace.web.seller.service.StatsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -19,31 +22,39 @@ import javax.servlet.http.HttpServletRequest;
 public class StatsController {
 
     private final StatsService statsService;
-    private final SoftwareService softwareService;
     private final CommonService commonService;
-
-    /**
-     * 요금계산 메인페이지
-     *
-     * @return
-     */
-    @GetMapping(value = "/list")
-    public String getStats() {
-        return "contents/priceCalculation";
-    }
 
     /**
      * 요금 목록 조회
      *
-     * @param httpServletRequest the httpServletRequest
-     * @return CustomPage<Software>
+     * @param httpServletRequest
+     * @return
      */
     @GetMapping
     @ResponseBody
-    public CustomPage<Stats> getStatsList(HttpServletRequest httpServletRequest){
-        return statsService.getStatsList(commonService.setParameters(httpServletRequest));
+    public CustomPage<Software> getSoftwareList(HttpServletRequest httpServletRequest){
+        return statsService.getSoftwareList(commonService.setParameters(httpServletRequest));
     }
 
-
+    /**
+     * 요금 목록 페이지 이동
+     *
+     * @param model
+     * @param oauth2User
+     * @param httpSession
+     * @param spec
+     * @param authentication
+     * @return
+     */
+    @GetMapping(value = "/softwares/my")
+    public String getStatsSoftwaresMy(Model model, @AuthenticationPrincipal OAuth2User oauth2User, HttpSession httpSession, SoftwareSpecification spec, Authentication authentication) {
+        log.info(">> stats Init" );
+        model.addAttribute("categories", statsService.getCategories());
+        model.addAttribute("status", Software.Status.values());
+        model.addAttribute("spec", new SoftwareSpecification());
+        model.addAttribute("types", Software.Type.values());
+        model.addAttribute("yns", Yn.values());
+        return "contents/priceCalculation";
+    }
 
 }
--- src/main/java/org/openpaas/paasta/marketplace/web/seller/service/StatsService.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/seller/service/StatsService.java
@@ -1,15 +1,19 @@
 package org.openpaas.paasta.marketplace.web.seller.service;
 
-import com.sun.org.glassfish.external.statistics.Stats;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.openpaas.paasta.marketplace.api.domain.Category;
 import org.openpaas.paasta.marketplace.api.domain.CustomPage;
+import org.openpaas.paasta.marketplace.api.domain.Software;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +23,20 @@ public class StatsService {
     @Autowired
     private final RestTemplate paasApiRest;
 
-    public CustomPage<Stats> getStatsList(String queryParamString) {
-        ResponseEntity<CustomPage<Stats>> responseEntity = paasApiRest.exchange("/profiles/page" + queryParamString, HttpMethod.GET, null, new ParameterizedTypeReference<CustomPage<Stats>>() {});
-        CustomPage<Stats> customPage = responseEntity.getBody();
+    @SneakyThrows
+    public List<Category> getCategories() {
+        return paasApiRest.getForObject("/categories", List.class);
+    }
+
+    //Page::Instance
+    public CustomPage<Software> getSoftwareList(String queryParamString) {
+        ResponseEntity<CustomPage<Software>> responseEntity = paasApiRest.exchange(" /stats/softwares/my" + queryParamString, HttpMethod.GET, null, new ParameterizedTypeReference<CustomPage<Software>>() {});
+        CustomPage<Software> customPage = responseEntity.getBody();
+
+        System.out.println("getContent ::: " + customPage.getContent());
+        System.out.println("getTotalElements ::: " + customPage.getTotalElements());
         return customPage;
     }
+
 
 }
